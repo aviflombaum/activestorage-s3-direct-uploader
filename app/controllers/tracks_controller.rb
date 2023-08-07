1483a5ecca34af411dd6da1fc5e20ebef837521a
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-08-07 08:31:21 -0400

    Create Track and attach audio_file

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -3,5 +3,16 @@ class TracksController < ApplicationController
   end
 
   def create
+    @track = Track.new(track_params)
+    @track.audio_file.attach(params[:signed_blob_id])
+    if @track.save
+      render json: @track, status: :created
+    end
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:title, :artist_name, :filename)
   end
 end
