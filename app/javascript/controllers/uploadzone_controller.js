6c5df3bbf1d5e522b40c7095dabfc2377a76ff33
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-08-07 06:08:07 -0400

    Drag and Drop files

--- app/javascript/controllers/uploadzone_controller.js
+++ app/javascript/controllers/uploadzone_controller.js
@@ -1,11 +1,29 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="uploadzone"
 export default class extends Controller {
   static targets = ["fileInput"];
-  connect() {}
+  connect() {
+    this.element.addEventListener("dragover", this.preventDragDefaults);
+    this.element.addEventListener("dragenter", this.preventDragDefaults);
+  }
+
+  disconnect() {
+    this.element.removeEventListener("dragover", this.preventDragDefaults);
+    this.element.removeEventListener("dragenter", this.preventDragDefaults);
+  }
+
+  preventDragDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
 
   trigger() {
     this.fileInputTarget.click();
+  }
+
+  acceptFiles(event) {
+    event.preventDefault();
+    const files = event.dataTransfer ? event.dataTransfer.files : event.target.files;
+    debugger;
   }
 }
