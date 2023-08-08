f7e9786bec5c7d112f5e9b9b759f797fff0a3cc6
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-08-08 08:49:57 -0400

    Implementing Upload

--- app/javascript/controllers/uploadzone_controller.js
+++ app/javascript/controllers/uploadzone_controller.js
@@ -4,12 +4,46 @@ import { post } from "@rails/request.js";
 
 class Upload {
   constructor(file) {
-    // Here's where we will instantiate the DirectUpload instance.
+    this.directUpload = new DirectUpload(file, "/rails/active_storage/direct_uploads");
   }
 
   process() {
-    // Here's where we will actually kick off the upload process.
-    console.log(this);
+    this.insertUpload();
+
+    this.directUpload.create((error, blob) => {
+      if (error) {
+        // Handle the error
+      } else {
+        const trackData = { track: { filename: blob.filename }, signed_blob_id: blob.signed_id };
+
+        post("/tracks", {
+          body: trackData,
+          contentType: "application/json",
+          responseKind: "json",
+        });
+      }
+    });
+  }
+
+  insertUpload() {
+    const fileUpload = document.createElement("div");
+
+    fileUpload.id = `upload_${this.directUpload.id}`;
+    fileUpload.className = "p-3 border-b";
+
+    fileUpload.textContent = this.directUpload.file.name;
+
+    const progressWrapper = document.createElement("div");
+    progressWrapper.className = "relative h-4 overflow-hidden rounded-full bg-secondary w-[100%]";
+    fileUpload.appendChild(progressWrapper);
+
+    const progressBar = document.createElement("div");
+    progressBar.className = "progress h-full w-full flex-1 bg-primary";
+    progressBar.style = "transform: translateX(-100%);";
+    progressWrapper.appendChild(progressBar);
+
+    const uploadList = document.querySelector("#uploads");
+    uploadList.appendChild(fileUpload);
   }
 }
 
@@ -40,46 +74,5 @@ export default class extends Controller {
     [...files].forEach((f) => {
       new Upload(f).process();
     });
-  }
-
-  uploadFile(file) {
-    const upload = new DirectUpload(file, "/rails/active_storage/direct_uploads");
-
-    this.insertUpload(upload);
-
-    upload.create((error, blob) => {
-      if (error) {
-        // Handle the error
-      } else {
-        const trackData = { track: { filename: blob.filename }, signed_blob_id: blob.signed_id };
-
-        post("/tracks", {
-          body: trackData,
-          contentType: "application/json",
-          responseKind: "json",
-        });
-      }
-    });
-  }
-
-  insertUpload(upload) {
-    const fileUpload = document.createElement("div");
-
-    fileUpload.id = `upload_${upload.id}`;
-    fileUpload.className = "p-3 border-b";
-
-    fileUpload.textContent = upload.file.name;
-
-    const progressWrapper = document.createElement("div");
-    progressWrapper.className = "relative h-4 overflow-hidden rounded-full bg-secondary w-[100%]";
-    fileUpload.appendChild(progressWrapper);
-
-    const progressBar = document.createElement("div");
-    progressBar.className = "progress h-full w-full flex-1 bg-primary";
-    progressBar.style = "transform: translateX(-100%);";
-    progressWrapper.appendChild(progressBar);
-
-    const uploadList = document.querySelector("#uploads");
-    uploadList.appendChild(fileUpload);
   }
 }
